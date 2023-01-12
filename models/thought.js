98e92b1915182ca136032d5e6280f275216c7530
Jimmy Vela <jimmyvela06@.com>
2023-01-12 01:20:13 -0600

    modules/thought/reactions/index/user fix

---
+++ models/thought.js
@@ -0,0 +1,70 @@
+const { Schema, model, Types } = require("mongoose");
+const date = require("../utils/date");
+const date_format = require("../utils/date");
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxLength: 280,
+    },
+    username: {
+      type: String,
+      require: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => date_format(timestamp),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => date_format(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+
+    reactions: [reactionSchema],
+  },
+
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtuals("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const thought = model("thought", thoughtSchema);
+
+model.exports = thought;
